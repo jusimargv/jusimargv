c846c64a08c78e09e60bcec340e03dcb3849c580
jusimargv <jusimargv@gmail.com>
2024-05-14 13:06:18 -0300

    doc_update

---
+++ Funções de input e output.py
@@ -0,0 +1,11 @@
+# Exemplos da Função Input e output
+
+nome = input("Informe seu nome aqui:")
+sobrenome = input(" Informe seu sobrenome aqui:")
+print(f"Usuário:", nome, sobrenome)
+
+# Exemplos
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="... \n")
+print(nome, sobrenome, sep="#")
---
+++ operadores_aritmeticos.py
@@ -0,0 +1,44 @@
+# Adição
+print(1 + 1)
+
+# Subtração
+print(10 - 2)
+
+# Multiplicação
+print(2 * 3 // 5 - 10 + 32 * 7)
+
+# divisão Inteira
+print(10 // 2)
+
+# divisão
+print(10 / 2)
+
+# Módulo
+print(10 % 2)
+
+# Exponenciação
+print(2 ** 3)
+
+# Exemplos:
+
+print(10 - 5 * 2)
+
+print((10-5)* 2)
+
+print(10 ** 2 * 2)
+
+print(10**(2 * 2))
+
+print(10 / 2 * 4)
+
+produto_1 = 10
+produto_2 = 20
+
+print(produto_1 // produto_2)
+
+print(produto_2 - produto_1 * (50))
+
+x =(10 + 5) * 4
+y =((10 // 2) *(25 * 2) - (2 ** 2))
+print(x)
+print(y)
