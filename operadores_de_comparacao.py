4e83a9a99fa70745e9919adc3e49958d14d86b06
jusimargv <jusimargv@gmail.com>
2024-05-15 12:44:40 -0300

    Doc_update

---
+++ operadores_de_comparacao.py
@@ -0,0 +1,25 @@
+# igualdade
+
+saldo = 450
+saque = 200
+print(saldo == saque)
+
+# diferença
+
+saldo = 450
+saque = 200
+print(saldo != saque)
+
+# maior que / maior ou igual
+saldo = 450
+saque = 200
+print(saldo > saque)
+
+print(saldo >= saque)
+
+# menor que / menor ou igual
+saldo = 450
+saque = 200
+print(saldo < saque)
+
+print(saldo <= saque)
