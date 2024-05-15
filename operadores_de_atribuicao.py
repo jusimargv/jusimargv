09dd31ebb82f8aa816342462ae81c5ff2dcb4aa9
jusimargv <jusimargv@gmail.com>
2024-05-15 14:37:26 -0300

    Doc_updade

---
+++ operadores_de_atribuicao.py
@@ -0,0 +1,60 @@
+# Atribuição Simples
+saldo = 500
+print("Seu Saldo é:",saldo)
+## Seu Saldo é: 500 // print
+
+# Atribuição com adição
+saldo =500
+saldo += 200
+
+print("Seu Saldo é:",saldo)
+
+saldo = 500
+desposito =400
+
+print("Seu Saldo é:",saldo + desposito)
+
+saldo = saldo + 200
+print("Seu Saldo é:",saldo)
+
+# Atribuição com subtração
+saldo = 500
+saldo -= 200
+
+print("Seu Saldo é:",saldo)
+
+
+saldo = 500
+saque = 200
+extrato = saldo + desposito - saque
+print("Seu Saldo é:",extrato)
+print("Seu Saldo é:",saldo - saque)
+
+
+saldo = saldo - 200
+print("Seu Saldo é:",saldo)
+
+# Atribuição com Multiplicação
+saldo = 500
+saldo *= 2
+print("Seu Saldo é:",saldo)
+
+# Atribuição com Divisão
+saldo = 500
+saldo /= 5
+print("Seu Saldo é:",saldo)
+
+saldo = 500
+saldo //= 5
+print("Seu Saldo é:",saldo)
+
+# Atribuição com Módulo
+
+saldo =500
+saldo %= 480
+print("Seu Saldo é:",saldo)
+
+# Atribuição com exponecisção
+saldo = 500 
+saldo **= 3
+print("Seu Saldo é:",saldo)
