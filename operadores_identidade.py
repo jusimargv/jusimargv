01e1f8388c28c26db79c183ea562cf084a76ad11
jusimargv <jusimargv@gmail.comgit config --global user.email jusimargv@gmail.com>
2024-05-16 14:27:20 -0300

    Doc_update

---
+++ operadores_identidade.py
@@ -0,0 +1,22 @@
+curso = "Curso de Python"
+nome_curso = curso
+saldo, limite = 200, 200
+
+curso is nome_curso
+
+curso is not nome_curso
+
+saldo is limite
+
+saldo = 1000
+
+limite = 500
+
+saque = 1000
+print(saldo is limite)
+
+print(saldo is not limite)
+
+print(saldo is limite is not saque)
+
+print(saldo is not limite)
