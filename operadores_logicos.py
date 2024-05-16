55d5db1346dc3e8b31449133512e11396c6c572d
jusimargv <jusimargv@gmail.com>
2024-05-16 10:35:24 -0300

    Doc_update

---
+++ operadores_logicos.py
@@ -0,0 +1,22 @@
+# operador AND
+saldo = 1000
+saque = 200
+limite = 100
+
+print(saldo >= saque)
+
+print(saque<= limite)
+
+print(saldo>= saque and saque <= limite)
+
+#operador OR
+
+saldo = 1000
+saque = 200
+limite = 100
+
+print(saldo >= saque)
+
+print(saque<= limite)
+
+print(saldo>= saque or saque <= limite)
--- test.js
+++
@@ -1,8 +0,0 @@
-let a = 20;
-let b = 4;
-let c = 12;
-
-
-let x = a - (-b * c);
-
-console.log( "resultado:", x);
