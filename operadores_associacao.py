f0a69227141ae9179cc67f3735db56a4fe30862d
jusimargv <jusimargv@gmail.comgit config --global user.email jusimargv@gmail.com>
2024-05-16 19:38:16 -0300

    Doc_update

---
+++ operadores_associacao.py
@@ -0,0 +1,9 @@
+curso = "Curso de Python"
+frutas = ["laranja", "uva","limão"]
+saques = [1500, 100]
+
+print("Python"in curso)
+
+print("maçã" not in frutas)
+
+print(200 in saques)
--- operadores_identidade.py
+++ operadores_identidade.py
@@ -21,5 +21,3 @@ print(saldo is limite is not saque)
 
 print(saldo is not limite)
 
-x =(22-10) * 3
-print(x)
